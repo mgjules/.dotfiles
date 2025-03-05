2d5c0cdfc5f64897fedeb555248d3d4f864a00aa
Michaël Giovanni Jules <julesmichaelgiovanni@gmail.com>
2025-03-05 12:09:01 +0400

    no need for eslint auto format

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -8,7 +8,7 @@ vim.opt.scrolloff = 10
 vim.opt.relativenumber = false
 
 -- Set to false to disable auto format
-vim.g.lazyvim_eslint_auto_format = true
+vim.g.lazyvim_eslint_auto_format = false
 
 -- Enable the option to require a Prettier config file
 -- If no prettier config file is found, the formatter will not be used
