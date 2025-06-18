3f5f24489f87d8124073632057561e997c6de518
Michaël Giovanni Jules <julesmichaelgiovanni@gmail.com>
2025-06-18 11:27:31 +0400

    adjust bigfile

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -3,8 +3,8 @@ return {
   ---@type snacks.Config
   opts = {
     bigfile = {
-      size = 10 * 1024 * 1024, -- 10MB
-      line_length = 5000, -- average line length (useful for minified files)
+      size = 100 * 1024 * 1024, -- 10MB
+      line_length = 5000000, -- average line length (useful for minified files)
     },
   },
 }
