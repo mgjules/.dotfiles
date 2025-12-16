11a6664f7e8c6d91e38d8ea79cd2652a0be8d133
Michaël Giovanni Jules <julesmichaelgiovanni@gmail.com>
2025-12-17 00:41:54 +0400

    required change moving to 15.x

--- .config/nvim/lua/plugins/disabled.lua
+++ .config/nvim/lua/plugins/disabled.lua
@@ -1,7 +1,7 @@
 return {
   { "akinsho/bufferline.nvim", enabled = false },
   { "nvim-treesitter/nvim-treesitter-context", enabled = false },
-  { "echasnovski/mini.indentscope", enabled = false },
+  { "nvim-mini/mini.indentscope", enabled = false },
   { "catppuccin/nvim", enabled = false },
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
 }
